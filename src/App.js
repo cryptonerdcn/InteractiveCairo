bc478dbbeda10e0cbc63dd03040b9d6e06034aaf
biyu <y-bi@unext.jp>
2023-02-18 15:55:54 +0900

    Init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import logo from './logo.svg';
 import './App.css';
+import CodeEditor from '@uiw/react-textarea-code-editor';
 
 function App() {
+  const [code, setCode] = React.useState(
+      `function add(a, b) {\n  return a + b;\n}`
+  );
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+
+
+      <CodeEditor
+          className="Editor"
+          value={code}
+          language="rust"
+          placeholder="Please enter Cairo code."
+          onChange={(evn) => setCode(evn.target.value)}
+          padding={15}
+          style={{
+            fontSize: 12,
+            backgroundColor: "#f5f5f5",
+            fontFamily: 'ui-monospace,SFMono-Regular,SF Mono,Consolas,Liberation Mono,Menlo,monospace',
+          }}
+      />
+
+      </div>
   );
 }
 
